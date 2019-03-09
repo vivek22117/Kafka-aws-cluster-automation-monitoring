208607a3f54dc5848ba0a11c406f42b89c92ced9
Vivek Mishra <vivekmishra22117@gmail.com>
2019-03-09 23:18:44 +0530

    Added file for kafa and Zookee

--- Jenkins/Jenkinsfile.groovy
+++ Jenkins/Jenkinsfile.groovy
@@ -55,6 +55,30 @@ pipeline {
         string(name: 'KAFKA_CLUSTER_STACK', defaultValue: 'kafka-cluster-resources', description: 'worspace to use in Terraform')
     }
     stages {
+        stage('Create-or-Destroy!') {
+            steps {
+                script {
+                    def apply = false
+                    try {
+                        input message: 'confirm apply', ok: 'Apply config'
+                        apply = true;
+                    } catch (err) {
+                        apply = false
+                        sh "aws cloudformation delete-stack --stack-name ${params.ZK_FX_STACK} --region ${params.REGION}"
+                        sh "aws cloudformation --region ${params.REGION} wait stack-delete-complete --stack-name ${params.ZK_FX_STACK}"
+                        sh "aws cloudformation delete-stack --stack-name ${params.KAFKA_FX_STACK} --region ${params.REGION}"
+                        sh "aws cloudformation --region ${params.REGION} wait stack-delete-complete --stack-name ${params.KAFKA_FX_STACK}"
+                        sh "aws cloudformation delete-stack --stack-name ${params.ZK_CLUSTER_STACK} --region ${params.REGION}"
+                        sh "aws cloudformation --region ${params.REGION} wait stack-delete-complete --stack-name ${params.ZK_CLUSTER_STACK}"
+                        sh "aws cloudformation delete-stack --stack-name ${params.KAFKA_CLUSTER_STACK} --region ${params.REGION}"
+                        sh "aws cloudformation --region ${params.REGION} wait stack-delete-complete --stack-name ${params.KAFKA_CLUSTER_STACK}"
+                    }
+                    if (apply) {
+                        sh "echo Creating AWS infrastructure......!"
+                    }
+                }
+            }
+        }
         stage('zookeeper-fixed-resources-infra') {
             steps {
                 dir('aws-cloudformation/infra/zookeeper-cluster/') {
@@ -69,13 +93,13 @@ pipeline {
                             if (status == 'DELETE_FAILED' || 'ROLLBACK_COMPLETE' || 'ROLLBACK_FAILED' || 'UPDATE_ROLLBACK_FAILED') {
                                 sh "aws cloudformation delete-stack --stack-name ${params.ZK_FX_STACK} --region ${params.REGION}"
                                 sh 'echo Creating Zookeeper Cluster....'
-                                createZookeeperClusterStack(${params.REGION}, ${params.ZK_FX_STACK})
+                                createZookeeperClusterStack($ { params.REGION }, $ { params.ZK_FX_STACK })
                                 apply = false
                             }
                         } catch (err) {
                             apply = false
                             sh "echo Creating Zookeeper Cluster"
-                            createZookeeperFixedStack(${ params.REGION }, ${ params.ZK_FX_STACK })
+                            createZookeeperFixedStack($ { params.REGION }, $ { params.ZK_FX_STACK })
                         }
                         if (apply) {
                             try {
@@ -106,13 +130,13 @@ pipeline {
                             if (status == 'DELETE_FAILED' || 'ROLLBACK_COMPLETE' || 'ROLLBACK_FAILED' || 'UPDATE_ROLLBACK_FAILED') {
                                 sh "aws cloudformation delete-stack --stack-name ${params.KAFKA_FX_STACK} --region ${params.REGION}"
                                 sh 'echo Creating Zookeeper Cluster....'
-                                createKafkaFixedStack(${ params.REGION }, ${ params.KAFKA_FX_STACK })
+                                createKafkaFixedStack($ { params.REGION }, $ { params.KAFKA_FX_STACK })
                                 apply = false
                             }
                         } catch (err) {
                             apply = false
                             sh 'echo Creating Kafka Fixed resources....'
-                            createKafkaFixedStack(${params.REGION}, ${params.KAFKA_FX_STACK})
+                            createKafkaFixedStack($ { params.REGION }, $ { params.KAFKA_FX_STACK })
                         }
                         if (apply) {
                             try {
@@ -143,13 +167,13 @@ pipeline {
                             if (status == 'DELETE_FAILED' || 'ROLLBACK_COMPLETE' || 'ROLLBACK_FAILED' || 'UPDATE_ROLLBACK_FAILED') {
                                 sh "aws cloudformation delete-stack --stack-name ${params.ZK_CLUSTER_STACK} --region ${params.REGION}"
                                 sh 'echo Creating Zookeeper Cluster....'
-                                createZookeeperClusterStack(${ params.REGION }, ${ params.ZK_CLUSTER_STACK })
+                                createZookeeperClusterStack($ { params.REGION }, $ { params.ZK_CLUSTER_STACK })
                                 apply = false
                             }
                         } catch (err) {
                             apply = false
                             sh 'echo Creating Zookeeper Cluster for first time....'
-                            createZookeeperClusterStack(${ params.REGION }, ${ params.ZK_CLUSTER_STACK })
+                            createZookeeperClusterStack($ { params.REGION }, $ { params.ZK_CLUSTER_STACK })
                         }
                         if (apply) {
                             try {
@@ -180,13 +204,13 @@ pipeline {
                             if (status == 'DELETE_FAILED' || 'ROLLBACK_COMPLETE' || 'ROLLBACK_FAILED' || 'UPDATE_ROLLBACK_FAILED') {
                                 sh "aws cloudformation delete-stack --stack-name ${params.KAKFA_CLUSTER_STACK} --region ${params.REGION}"
                                 sh 'echo Creating Zookeeper Cluster....'
-                                createKafkaClusterStack(${ params.REGION }, ${ params.KAKFA_CLUSTER_STACK })
+                                createKafkaClusterStack($ { params.REGION }, $ { params.KAKFA_CLUSTER_STACK })
                                 apply = false
                             }
                         } catch (err) {
                             apply = false
                             sh 'echo Creating Zookeeper Cluster for first time....'
-                            createKafkaClusterStack(${ params.REGION }, ${ params.KAKFA_CLUSTER_STACK })
+                            createKafkaClusterStack($ { params.REGION }, $ { params.KAKFA_CLUSTER_STACK })
                         }
                         if (apply) {
                             try {
